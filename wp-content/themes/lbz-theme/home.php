7a177c39dd79f495efdd6ea8dffb6c34428781bd
Moacir-DEV <moacirjunior.eletro@gmail.com>
2018-04-22 20:13:20 -0300

    Corrigido bug da tela de produtos

--- wp-content/themes/lbz-theme/home.php
+++ wp-content/themes/lbz-theme/home.php
@@ -1,25 +1,59 @@
 <?php get_header(); ?>
 
-<h1>Notícias</h1>
-
 <div class="row">
-	<div class="col-md-8">
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    
+    <div class="col-md-9">
+        <h1 id="page-title">Produtos</h1>
+        
+        <?php $count = 1 ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-		    <div class="entry-content">
-		        <?php echo ucfirst(get_the_time('l, j \d\e F \d\e Y')); ?>
-		    </div>
-		    <hr>
+            <!--<h2><a href="<?php //the_permalink() ?>"><?php //the_title(); ?></a></h2>
+            <div class="entry-content">
+                <?php //echo ucfirst(get_the_time('l, j \d\e F \d\e Y')); ?>
+            </div>
+            <hr>-->
+            
+            
+            <div class="widget">    
+                <div class="widget-title 
+                     <?php if (($count % 2) == 1) { echo ""; } else { echo "flex-reverse";} ?>">
+                    <div class="title-space"></div>
+                    <h3><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h3>
+                </div>
 
-		<?php endwhile; else: ?>
-		    <p><?php _e('Desculpe, esta página não existe.'); ?></p>
-		<?php endif; ?>
-	</div>
+                <div class="widget-content 
+                     <?php if (($count % 2) == 1) { echo ""; } else { echo "flex-reverse";} ?>">
+                    <div class="widget-img">
+                        <picture>
+                            <?php $img_directory = esc_url(home_url('/')) . "wp-content/themes/lbz-theme/images/"; ?>
+                            <img src="
+                            <?php 
+                                if (($count % 2) == 1) {
+                                    echo $img_directory, 'widget-servicos.png';
+                                }
+                                else {
+                                    echo $img_directory, 'widget-produtos.png';
+                                }
+                            ?>">
+                        </picture>
+                    </div>
 
-	<div class="col-md-4">
-		<?php get_sidebar() ?>
-	</div>
+                    <div class="widget-text">
+                        <p><?php the_content(); ?></p>
+                        <small><?php echo ucfirst(get_the_time('l, j \d\e F \d\e Y')); ?></small>
+                    </div>
+                </div>
+            </div>
+        <?php $count++ ?>
+        <?php endwhile; else: ?>
+            <p><?php _e('Desculpe, ainda não temos produtos cadastrados.'); ?></p>
+        <?php endif; ?>
+    </div>
+    
+    <div class="col-md-3">
+        <?php get_sidebar() ?>
+    </div>
 </div>
 
 <?php get_footer(); ?>
