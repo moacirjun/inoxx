f832f3602c988f230eecae9db4c68e1a728733ab
MoacirjuN <moacirjunior.eletro@gmail.com>
2018-04-23 11:58:37 -0300

    incluí o Scroll automático para o título da página

--- wp-content/themes/lbz-theme/lib/js/main.js
+++ wp-content/themes/lbz-theme/lib/js/main.js
@@ -1,5 +1,15 @@
 window.onscroll = function() {navScroll();};
 
+window.onload = function() {
+    var pageTitle = document.querySelector("#page-title"),
+            vTop = document.querySelector("header").scrollHeight;
+    if (pageTitle === null || pageTitle === 'undefined') {
+        return;
+    }
+    
+    window.scroll({top: vTop, left:0, behavior:'smooth'});
+}
+
 function navScroll() {
     if (window.screen.width <= 990) {
         return;
@@ -7,13 +17,12 @@ function navScroll() {
     
     var menu = document.querySelector("nav"),
             navBrand = document.querySelector(".navbar-brand"),
-            navBrandImg = document.querySelector(".navbar-brand img"),
-            classNameDefault = "navbar navbar-expand-lg fixed-top ";
+            navBrandImg = document.querySelector(".navbar-brand img")
     
     if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         menu.classList.replace("navbar-light", "navbar-dark");
         menu.classList.replace("bg-transp", "bg-azul-marinho");
-        menu.style.transition = "all 500ms";
+        menu.style.transition = "all 300ms";
         navBrand.classList.add("mobile");
         navBrandImg.classList.add("mobile");
     } else {
@@ -21,8 +30,8 @@ function navScroll() {
         menu.classList.replace("bg-azul-marinho", "bg-transp");
         navBrand.classList.remove("mobile");
         navBrandImg.classList.remove("mobile");
-        navBrand.style.transition = "all 500ms";
-        navBrandImg.style.transition = "all 500ms";
+        navBrand.style.transition = "all 300ms";
+        navBrandImg.style.transition = "all 300ms";
     }
 }
 
