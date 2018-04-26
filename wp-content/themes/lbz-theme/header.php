fa180e262f01eef2225a9f62b5eafdf9f5e36d4d
MoacirjuN <moacirjunior.eletro@gmail.com>
2018-04-26 19:02:05 -0300

    estlilos

--- wp-content/themes/lbz-theme/header.php
+++ wp-content/themes/lbz-theme/header.php
@@ -76,10 +76,16 @@
                 </div>
                 <a class="carousel-control-prev" href="#slide-produtos" 
                    role="button"data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
                 </a>
                 <a class="carousel-control-next" href="#slide-produtos"
                    role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
                 </a>
+                    
+              </a>
             </div>
         </div>
             
